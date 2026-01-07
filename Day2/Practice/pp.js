4fd495554b067e75fcbef9f97d2d942ee67ae22c
Karnapuumamaheswari <karnapuumamaheswari022005@gmail.com>
2026-01-07 17:43:34 +0530

    Pushed the practice of day2

--- Day2/Practice/pp.js
+++ Day2/Practice/pp.js
@@ -1,2 +1,12 @@
-console.log("Hello, World!");
-console.log("This is a practice file for Day 2.");
+const fs = require("fs");
+const path = require("path");
+
+let readFilePath = path.join(__dirname, "data.txt");
+let writeFilePath = path.join(__dirname, "data1.txt");
+
+let d = fs.readFileSync(readFilePath, "utf-8");
+console.log("Content from data.txt:");
+console.log(d);
+
+fs.writeFileSync(writeFilePath, "This is new content added to the file.");
+console.log("Successfully wrote to data1.txt");
